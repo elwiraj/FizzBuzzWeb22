2cabd5d48a931399c8ab3f9d9abc59efa7389bf9
Elwira Jaroć <ekwa5757@gfmail.com>
2023-03-29 10:53:56 +0200

    Dodaj pliki projektów.

---
+++ Forms/FizzBuzzForm.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace FizzBuzzWeb2.Forms
+{
+    public class FizzBuzzForm
+    {
+        [Display(Name = "Twój szczęśliwy numerek")]
+        [Range(1, 1000, ErrorMessage = "Oczekiwana wartość {0} z zakresu {1} i {2}.")] 
+        [Required(ErrorMessage= "Pole jest obowiązkowe")]
+        public int? Number { get; set; }
+        public string? Response { get; set; } = "";
+
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using FizzBuzzWeb2.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FizzBuzzWeb2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty(SupportsGet = true)]
+
+        public FizzBuzzForm FizzBuzz { get; set; } 
+        [BindProperty(SupportsGet = true)]
+        public string Name {  get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                
+            }
+            UpdateResponse();
+            return Page();
+            return RedirectToPage("./Privacy");
+        }
+
+        public void UpdateResponse()
+        {
+            if(FizzBuzz.Number%3==0 && FizzBuzz.Number%5==0)
+            {
+                FizzBuzz.Response = "FizzBuzz";
+            }
+            else if(FizzBuzz.Number%3==0)
+            {
+                FizzBuzz.Response = "Fizz";
+            }
+            else if(FizzBuzz.Number%5==0)
+            {
+                FizzBuzz.Response = "Buzz";
+            }
+            else
+            {
+                FizzBuzz.Response = "Liczba: " + FizzBuzz.Number + " nie spełnia kryteriów FizzBuzz";
+            }
+        }
+
+        public void OnGet()
+        {
+            if(string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+        }
+    }
+}
